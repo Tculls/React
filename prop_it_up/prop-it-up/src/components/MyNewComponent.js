db97937360e0632d4091d6ea31f6398d6e1d55ec
Tculls <tanner.culley@gmail.com>
2022-05-04 13:43:54 -0700

    commit

--- prop_it_up/prop-it-up/src/components/MyNewComponent.js
+++ prop_it_up/prop-it-up/src/components/MyNewComponent.js
@@ -1,24 +1,22 @@
-import React, {Component } from 'react';
+import React from 'react';
 
-class PersonComponent extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            age: this.props.age
-        }
-    }
-    birthday = () => {
-        this.setState({age: this.state.age += 1 })
-    }
-    render(){
-        return <div>
-            <h2>{this.props.lastName}, {this.props.firstName}</h2>
-            <p>Age: {this.state.age}</p>
-            <p>Hair Color: {this.props.hairColor}</p>
-            <button onClick={this.birthday}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+
+const PersonCard = props => {
+
+    
+    // birthday = () => {
+    //     this.setState({age: this.state.age + 1 })
+    // }
+    return (
+        <div>
+            <h2>{props.lastName}, {props.firstName}</h2>
+            <p>Age: {props.age}</p>
+            <p>Hair Color: {props.hairColor}</p>
+            {/* <button onClick={birthday}>Birthday Button for {this.props.firstName} {this.props.lastName}</button> */}
         </div>
-    }
+    );
+    
 }
 
 
-export default PersonComponent;
+export default PersonCard;
